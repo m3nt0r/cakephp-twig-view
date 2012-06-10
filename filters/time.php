2ea9f30bb738487c5e42f8df7ca9ecf17de1a95b
Kjell Bublitz <m3nt0r.de@gmail.com>
2012-06-10 21:10:16 +0200

    added gmt, rssTime and atomTime

--- filters/time.php
+++ filters/time.php
@@ -25,8 +25,12 @@ class Twig_Extension_Time extends Twig_Extension {
 	public function getFilters() {
 		return array(
 			'ago' => new Twig_Filter_Function('TwigView_Filter_Time::timeAgoInWords'),
+			'relative' => new Twig_Filter_Function('TwigView_Filter_Time::timeAgoInWords'),
 			'nice' => new Twig_Filter_Function('TwigView_Filter_Time::nice'),
 			'niceShort' => new Twig_Filter_Function('TwigView_Filter_Time::niceShort'),
+			'gmt' => new Twig_Filter_Function('TwigView_Filter_Time::gmt'),
+			'rssTime' => new Twig_Filter_Function('TwigView_Filter_Time::toRSS'),
+			'atomTime' => new Twig_Filter_Function('TwigView_Filter_Time::toAtom'),
 		);
 	}
 }
@@ -41,12 +45,51 @@ class TwigView_Filter_Time extends TwigView_Filter {
 	/**
 	 * TimeHelper::timeAgoInWords
 	 *
+	 * ### Options:
+	 *
+	 * - `format` => a fall back format if the relative time is longer than the duration specified by end
+	 * - `end` => The end of relative time telling
+	 * - `userOffset` => Users offset from GMT (in hours)
+	 *
 	 * @param string $var 
 	 * @return void
 	 * @author Kjell Bublitz
 	 */
-	function timeAgoInWords($var) {
-		return self::helperObject('TimeHelper')->timeAgoInWords($var);
+	function timeAgoInWords($var, $options = array()) {
+		return self::helperObject('TimeHelper')->timeAgoInWords($var, $options);
+	}
+		
+	/**
+	 * TimeHelper::toRSS
+	 *
+	 * @param string $var UNIX timestamp or a valid strtotime() date string
+	 * @return void Date formatted for RSS feeds
+	 * @author Kjell Bublitz
+	 */
+	function toRSS($var, $userOffset = null) {
+		return self::helperObject('TimeHelper')->toRSS($var, $userOffset);
+	}
+	
+	/**
+	 * TimeHelper::toAtom
+	 *
+	 * @param string $var UNIX timestamp or a valid strtotime() date string
+	 * @return void Date formatted for Atom RSS feeds
+	 * @author Kjell Bublitz
+	 */
+	function toAtom($var, $userOffset = null) {
+		return self::helperObject('TimeHelper')->toAtom($var, $userOffset);
+	}
+	
+	/**
+	 * TimeHelper::gmt
+	 *
+	 * @param string $var UNIX timestamp or a valid strtotime() date string
+	 * @return void
+	 * @author Kjell Bublitz
+	 */
+	function gmt($var) {
+		return self::helperObject('TimeHelper')->gmt($var);
 	}
 
 	/**
@@ -56,8 +99,8 @@ class TwigView_Filter_Time extends TwigView_Filter {
 	 * @return void
 	 * @author Kjell Bublitz
 	 */
-	function nice($var) {
-		return self::helperObject('TimeHelper')->nice($var);
+	function nice($var, $userOffset = null) {
+		return self::helperObject('TimeHelper')->nice($var, $userOffset);
 	}
 
 	/**
@@ -67,8 +110,8 @@ class TwigView_Filter_Time extends TwigView_Filter {
 	 * @return void
 	 * @author Kjell Bublitz
 	 */
-	function niceShort($var) {
-		return self::helperObject('TimeHelper')->niceShort($var);
+	function niceShort($var, $userOffset = null) {
+		return self::helperObject('TimeHelper')->niceShort($var, $userOffset);
 	}
 }
 ?>
